d8b789d7e08883ac21ab9e122225f83e11da910a
carter-vin <cartervincent684@gmail.com>
2023-11-11 05:15:00 +0500

    Show histogram and data

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,10 +2,13 @@
 
 import React, { useState } from 'react';
 import styles from '../styles/Home.module.css'; // Make sure to create this CSS module.
+import { Bar } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
 
 export default function Home() {
   const [file, setFile] = useState(null);
   const [analysisResults, setAnalysisResults] = useState(null);
+  const [isSequenceVisible, setIsSequenceVisible] = useState(false);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
@@ -29,7 +32,6 @@ export default function Home() {
         }
 
         const data = await response.json();
-        console.log(data)
         setAnalysisResults(data);  // Assuming you have a state variable to store the results
       } catch (error) {
         console.error("Failed to analyze the file:", error);
@@ -43,6 +45,34 @@ export default function Home() {
     const parts = fileName.split('.');
     return parts[parts.length - 1];
   }
+
+  const histogramChart = analysisResults && analysisResults.histogram_data ? (
+    <Bar
+      data={{
+        labels: Object.keys(analysisResults.histogram_data),
+        datasets: [{
+          label: 'Read Lengths',
+          data: Object.values(analysisResults.histogram_data),
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1,
+        }],
+      }}
+      options={{
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      }}
+    />
+  ) : null;
+
+  const formatSequence = (sequence) => {
+    const chunkSize = 50; // Adjust the chunk size as needed
+    const regex = new RegExp(`.{1,${chunkSize}}`, 'g');
+    return sequence.match(regex).join(' ');
+  };
 
   return (
     <div className={styles.container}>
@@ -60,6 +90,33 @@ export default function Home() {
         </div>
         <button type="submit" className={styles.button}>Analyze File</button>
       </form>
+      {analysisResults && (
+        <div className={styles.results}>
+          <h2 className={styles.margins}>Analysis Results</h2>
+          <p className={styles.margins}>Sequence Length: {analysisResults.sequence_length}</p>
+          <p className={styles.margins}>G/C Content: {analysisResults.gc_content.toFixed(2)}%</p>
+          <div className={styles.margins}>
+            <button onClick={() => setIsSequenceVisible(!isSequenceVisible)}>
+              {isSequenceVisible ? 'Hide' : 'Show'} Reverse Complement
+            </button>
+            {isSequenceVisible && <p>Reverse Complement: {formatSequence(analysisResults.reverse_complement)}</p>}
+          </div>
+
+          {/* Conditionally display BAM file specific results */}
+          {analysisResults.reads_count && (
+            <>
+              <p className={styles.margins}>Reads Count: {analysisResults.reads_count}</p>
+              {/* Optionally display histogram data */}
+              {analysisResults.histogram_data && (
+                <div className={styles.margins}>
+                  <h3>Histogram of Read Lengths</h3>
+                  {histogramChart}
+                </div>
+              )}
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
